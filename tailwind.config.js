bce1e4f3d5f02f23f9d869979fa84e279ad057b2
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-18 15:13:50 -0400

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+        colors: {
+          'DarkCyan': '#0E8784',
+          'DarkGreyBlue': '#333D4B',
+          'PaleOrange': '#FDD6BA',
+          'LightCream(BG)':'#FEFCF7',
+          'Grey': '#83888F'
+        },
+        FontFamily:{
+          Fraunces: ['Fraunces', 'serif'],
+          Barlow: ['Barlow', 'sans-serif']
+        },
+        spacing: {
+          
+        },
+    },
+  },
+  plugins: [],
+}
